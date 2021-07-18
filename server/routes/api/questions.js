cd06a3081cb3be1ab9760c45d9bbc214511718c8
Sara <sarayaoliu@gmail.com>
2021-07-18 09:09:22 -0400

    Shuffle questions array randomly with Fisher-Yates algorithm

--- server/routes/api/questions.js
+++ server/routes/api/questions.js
@@ -10,110 +10,111 @@
  * data from mongodb and return appropriate results
  */
 
-const express = require('express');
-const router = express.Router();
-
+ const express = require('express');
+ const router = express.Router();
+ 
 // Question Data
-const Questions = require('../../models/questions-data.json')
+var Questions = require('../../models/questions-data.json')
 // Hint: get a bonus task here
 const shuffleArray = require('../../utils/shuffle');
-
-
-
-/**
- * Route details
- * api GET /api/questions
- * Description: Get all questions in the database
- * IMPORTANT: remove the answers from it's data
- * we don't want the client to know the answer.
- * 
- * Structure of the return JSON:
- * [
- *    {
- *      question: 'sample question',
- *      options: [
- *        'option1',
- *        'option2'
- *      ],
- *      id: '1234'
- *    }
- * ]
- * 
- */
-router.get('/', (req, res) => {
-  // Remove the lines below and write your implementation
-  res.status(200).send(Questions)
-})
-
-/**
- * Route details
- * api GET /api/questions/count
- * Description: This will get the count of the questions
- * from the database and return it 
- * Structure of the return JSON:
- * {
- *  count: 4
- * }
- */
- let count = Object.keys(Questions).length;
- console.log(Questions.length, "works line 64")
- router.get('/count', (req, res) => {
+console.log("Questions before", Questions);
+Questions = shuffleArray(Questions);
+console.log("Questions after", Questions);
+ 
+ /**
+  * Route details
+  * api GET /api/questions
+  * Description: Get all questions in the database
+  * IMPORTANT: remove the answers from it's data
+  * we don't want the client to know the answer.
+  * 
+  * Structure of the return JSON:
+  * [
+  *    {
+  *      question: 'sample question',
+  *      options: [
+  *        'option1',
+  *        'option2'
+  *      ],
+  *      id: '1234'
+  *    }
+  * ]
+  * 
+  */
+ router.get('/', (req, res) => {
    // Remove the lines below and write your implementation
-   var count = Questions.length;
-   var countReturn = {"count":count};
-  
-   res.send(countReturn)
+   res.status(200).send(Questions)
  })
-
-/**
- * Route details
- * api GET /api/questions/:qId
- * Description: This will get one question given the question ID
- * Structure of the return JSON:
- * {
- *    question: 'sample question',
- *    options: [
- *      'option1',
- *      'option2'
- *    ],
- *    id: '1234'
- * }
- */
-router.get('/:qId', (req, res) => {
-  // Remove the lines below and write your implementation
-  res.status(500).send({
-    error: 'not implemented'
+ 
+ /**
+  * Route details
+  * api GET /api/questions/count
+  * Description: This will get the count of the questions
+  * from the database and return it 
+  * Structure of the return JSON:
+  * {
+  *  count: 4
+  * }
+  */
+  let count = Object.keys(Questions).length;
+  console.log(Questions.length, "works line 64")
+  router.get('/count', (req, res) => {
+    // Remove the lines below and write your implementation
+    var count = Questions.length;
+    var countReturn = {"count":count};
+   
+    res.send(countReturn)
   })
-})
-
-
-/**
- * Route details
- * api POST /api/questions/result
- * Description: This will receive a body with user
- * entered answers and will return the results. 
- * Calculation of the result will happen here and you
- * would only send the results.
- * 
- * Structure of body JSON:
- * {
- *    'questionID': 'user-answer',
- *    'questionID': 'user-answer'
- * }
- * 
- * Structure of the return JSON:
- * {
- *    summary: 'passed OR failed',
- *    score: (how many answers were correct),
- *    total: (how many questions were there)
- * }
- */
-router.post('/result', (req, res) => {
-  // Remove the lines below and write your implementation
-  res.status(500).send({
-    error: 'not implemented'
-  })
-})
-
-
-module.exports = router;
+ 
+ /**
+  * Route details
+  * api GET /api/questions/:qId
+  * Description: This will get one question given the question ID
+  * Structure of the return JSON:
+  * {
+  *    question: 'sample question',
+  *    options: [
+  *      'option1',
+  *      'option2'
+  *    ],
+  *    id: '1234'
+  * }
+  */
+ router.get('/:qId', (req, res) => {
+   // Remove the lines below and write your implementation
+   res.status(500).send({
+     error: 'not implemented'
+   })
+ })
+ 
+ 
+ /**
+  * Route details
+  * api POST /api/questions/result
+  * Description: This will receive a body with user
+  * entered answers and will return the results. 
+  * Calculation of the result will happen here and you
+  * would only send the results.
+  * 
+  * Structure of body JSON:
+  * {
+  *    'questionID': 'user-answer',
+  *    'questionID': 'user-answer'
+  * }
+  * 
+  * Structure of the return JSON:
+  * {
+  *    summary: 'passed OR failed',
+  *    score: (how many answers were correct),
+  *    total: (how many questions were there)
+  * }
+  */
+ router.post('/result', (req, res) => {
+   // Remove the lines below and write your implementation
+   res.status(500).send({
+     error: 'not implemented'
+   })
+ })
+ 
+ 
+ module.exports = router;
